b55f47437d0c5d11c8f2c1cd59c22c6ceb6eadb6
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2020-09-23 20:45:08 -0500

    JSON to individual files

---
+++ dataBuild.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 22 12:23:39 2020
+
+@author: ankit aich
+
+data builder to separate json file into multiple text files 
+"""
+
+import json
+
+def getData():
+    with open('dataset.json') as file:
+        data = json.load(file)
+        temp= data['article_detail']
+    return temp
+
+def getFileName(dataPoint):
+    return dataPoint['Disease']
+
+def build(dataArray):
+    fileNum = 0
+    for each_data_point in dataArray:
+        name = getFileName(each_data_point)+str(fileNum)
+        path = 'data files/' + name + '.txt'
+        with open(path,'w') as tempWriter:
+            try:
+                tempWriter.write(each_data_point['HeadLine'])
+                tempWriter.write("\n\n\n")
+                tempWriter.write(each_data_point['Text'])
+            except UnicodeEncodeError:
+                pass
+        fileNum = fileNum + 1
+        
+
+def main():
+    temp = getData()
+    data = temp[2:]
+    #eliminate sample data points of first two entries
+    build(data)
+    
+if __name__ == '__main__':
+    main()
